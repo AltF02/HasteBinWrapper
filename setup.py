7f9538d20b40177bf390d0006f13f09147072e47
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-19 21:02:29 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='HasteBinWrapper',  
+     version='0.2',
+     scripts=['HasteBin'] ,
+     author="Matthew",
+     description="A wrapper for hastebin",
+     url="https://github.com/DankDumpster/HasteBinWrapper",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
