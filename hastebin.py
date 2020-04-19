4849f3fc946658217cd7d544377d5bf0a5543d7f
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-19 20:26:26 +0200

    Update hastebin.py

--- hastebin.py
+++ hastebin.py
@@ -5,12 +5,16 @@ class Session:
 
     def __init__(self, links: bool = False):
         self.links = links
+        self.session = aiohttp.ClientSession()
 
-    async def post_content(self, payload: str):
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url='https://hastebin.com/documents', data=payload.encode('utf-8')) as resp:
-                key = await resp.json()
-                if self.links:
-                    return "https://hastebin.com/" + key["key"]
-                else:
-                    return key["key"]
+    async def upload(self, payload: str):
+        async with self.session.post(url='https://hastebin.com/documents', data=payload.encode('utf-8')) as resp:
+            key = await resp.json()
+            if self.links:
+                return "https://hastebin.com/" + key["key"]
+            else:
+                return key["key"]
+
+    async def read(self, payload: str):
+        async with self.session.get(url=payload) as resp:
+            return resp.text()
