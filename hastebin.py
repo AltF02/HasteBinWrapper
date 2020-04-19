e8ac9f7bd9d53057626986ce161177bdc45a9f22
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-19 19:49:18 +0200

    Create hastebin.py

---
+++ hastebin.py
@@ -0,0 +1,22 @@
+import aiohttp
+import json
+import asyncio
+
+
+class HasteBin:
+
+    def __init__(self, links: bool = False):
+        self.links = links
+
+    async def post_content(self, payload: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url='https://hastebin.com/documents', data=payload) as resp:
+                key = json.loads(await resp.text())
+                print(key)
+                print(key["key"])
+                if self.links:
+                    return "https://hastebin.com/" + key["key"]
+                else:
+                    return key["key"]
+                    
+                    
